11ef923b892632d1aba347d07c1670bd6b3bd5f8
Mohamed <mohamedsubhibouchi@gmail.com>
2020-02-15 17:47:13 +0100

    create insert data

---
+++ Drag and Drop/insertData.sql
@@ -0,0 +1,6 @@
+INSERT INTO TB_TODOS (TITLE, STATUS) values ('Buy bananas', 'open');
+INSERT INTO TB_TODOS (TITLE, STATUS) values ('Buy apple', 'open');
+INSERT INTO TB_TODOS (TITLE, STATUS) values ('Go to doctor', 'open');
+INSERT INTO TB_TODOS (TITLE, STATUS) values ('Call Mohamad', 'open');
+INSERT INTO TB_TODOS (TITLE, STATUS) values ('Go to Bank', 'open');
+INSERT INTO TB_TODOS (TITLE, STATUS) values ('Buy orange', 'open');
