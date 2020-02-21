038266ac7d99ff9d69ee25c084613a81c93a3fed
Mohamad <mohamedsubhibouchi@gmail.com>
2020-02-21 23:33:11 +0100

    Update script.js
    fix problem when drop item on the same region

--- Drag and Drop/script.js
+++ Drag and Drop/script.js
@@ -1,5 +1,5 @@
 function handleDrag(event) {
-    
+
     var data = {id: event.target.dataset.id, status: event.target.dataset.status};
     var json_data = JSON.stringify(data);
     
@@ -10,21 +10,50 @@ function handleDrag(event) {
 function handleDragover(event){
     event.preventDefault();
 }
-function handleDrop(event){
+
+
+function handleDropFinished(event){
+    
     var id = JSON.parse(event.dataTransfer.getData('data')).id;
     var status = JSON.parse(event.dataTransfer.getData('data')).status;
-    apex.server.process(
-        'Update Todo',
-        {
-            x01: status,
-            x02: id
-        },
-        {
-            success: function(pData){
-                apex.event.trigger('#open', 'apexrefresh');
-                apex.event.trigger('#finished', 'apexrefresh')
+    
+    if(status === 'open'){
+        apex.server.process(
+            'Update Todo',
+            {
+                x01: status,
+                x02: id
             },
-            dataType: 'text'
-        }
-    )
+            {
+                success: function(pData){
+                    apex.event.trigger('#open', 'apexrefresh');
+                    apex.event.trigger('#finished', 'apexrefresh');
+                },
+                dataType: 'text'
+            }
+        );
+    }
+}
+
+function handleDropOpen(event){
+    
+    var id = JSON.parse(event.dataTransfer.getData('data')).id;
+    var status = JSON.parse(event.dataTransfer.getData('data')).status;
+    
+    if (status === 'finished'){
+         apex.server.process(
+             'Update Todo',
+             {
+                 x01: status,
+                 x02: id
+             },
+             {
+                 success: function(pData){
+                     apex.event.trigger('#open', 'apexrefresh');
+                     apex.event.trigger('#finished', 'apexrefresh')
+                 },
+                 dataType: 'text'
+             }
+         );
+    }
 }
